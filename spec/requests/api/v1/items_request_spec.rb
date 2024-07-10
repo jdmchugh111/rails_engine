bade6b1268a671e2101d30e5e7e03c678f1ec670
James McHugh <jdmchugh111@gmail.com>
2024-07-10 03:55:53 -0400

    Wrote test for Edit an Item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -21,3 +21,20 @@ describe "Create an Item" do
     expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
+
+describe "Edit an Item" do
+  it "can update an item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant: merchant).id
+    previous_name = Item.last.name
+    item_params = { name: "Tam" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+  
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find(id)
+  
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Tam")
+  end
+end
