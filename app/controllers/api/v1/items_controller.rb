eb3636ddddc7aa4bb46d37f97ee5659bff18cf4c
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-07-09 22:04:40 -0600

    refactor

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    if params[:merchant_id]
+    if params[:merchant_id].present?
       render json: ItemSerializer.new(Item.merchant_items(params[:merchant_id]))
     else  
       render json: ItemSerializer.new(Item.all)
