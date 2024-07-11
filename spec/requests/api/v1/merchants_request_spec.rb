af7bb9f11b8f06f44321ca49bf747d6f583d2cb6
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-07-11 11:48:11 -0600

    add a sad path test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -54,3 +54,18 @@ describe "Get one Merchant" do
     expect(merchant[:attributes][:name]).to eq(merchant_1.name)
   end
 end
+
+describe "Get one Merchant sad path" do
+  it "will gracefully handle if a book id doesn't exist" do
+    get "/api/v1/merchants/1"
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:code]).to eq(404)
+    expect(data[:errors].first[:title]).to eq("Couldn't find Merchant with 'id'=1")
+  end
+end
